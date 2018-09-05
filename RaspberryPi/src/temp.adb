dae0e6887926464c30214d013f2c93d4bd7cbf43
Johannes Kliemann <jk@jkliemann.de>
2018-09-05 14:48:21 +0200

    adapt temperature example

--- RaspberryPi/src/temp.adb
+++ RaspberryPi/src/temp.adb
@@ -1,9 +1,10 @@
 with Ada.Text_Io;
 with Temperature;
+with Temperature.MicroBit;
 
 procedure Temp
 is
-   T : Temperature.Temp_Celcius := Temperature.Read;
+   T : Temperature.Temp_Celcius := Temperature.MicroBit.Read;
 begin
    Ada.Text_Io.Put_Line (Temperature.Temp_Celcius'Image (T));
 end Temp;
